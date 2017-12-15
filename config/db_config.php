a2edff5624d3dcc6ca2fc2903d4a0851c41f0188
Alan Coon <alancoon@usc.edu>
2017-12-14 18:32:10 -0800

    Add config/ and composer data.

---
+++ config/db_config.php
@@ -0,0 +1,6 @@
+<?php
+	define('DB_HOST', '303.itpwebdev.com');
+	define('DB_USER', 'alancoon_db_user');
+	define('DB_PASS', 'This is bullshit.');
+	define('DB_NAME', 'alancoon_spotidash_db');
+?>
---
+++ config/spotify_config.php
@@ -0,0 +1,5 @@
+<?php
+	define('CLIENT_ID', 'd4f3352fc37d4f8ea242f3a1bcc441b5');
+    define('CLIENT_SECRET', 'c98084da46c140a0a76d302e25db6d60');
+    define('REDIRECT_URI', 'http://303.itpwebdev.com/~alancoon/final/login.php');
+?>
